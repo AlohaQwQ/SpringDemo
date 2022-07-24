ac38f679ddf8106c1c868861b2cedfccaff47631
AlohaQwQ <757993938@qq.com>
2022-07-24 17:14:28 +0800

    Initial commit

---
+++ SpringDemo/src/main/java/com/example/MainApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Aloha
+ * @date 2022/7/24 17:11
+ * @description 这是一个SpringBoot主程序入口类
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
